5a5ec1c3ebe2e26158eb96aec57e110d53feeb64
Leonis0813 <Leonis.0813@gmail.com>
2015-12-20 15:16:41 +0900

    create environment for test

---
+++ RegisterTest/src/com/register/android/test/MainActivityTest.java
@@ -0,0 +1,11 @@
+package com.register.android.test;
+
+import com.register.android.MainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	public MainActivityTest(Class<MainActivity> activityClass) {
+		super(activityClass);
+	}
+}
