583330ed0c9f9e4849588dd6bea99f52cbc4c6cb
Leonis0813 <Leonis.0813@gmail.com>
2015-12-23 18:38:00 +0900

    update environment

---
+++ RegisterTest/src/com/register/android/test/HTTPClientTest.java
@@ -0,0 +1,14 @@
+package com.register.android.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HTTPClientTest {
+
+	@Test
+	public void testLoadInBackground() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ RegisterTest/src/com/register/android/test/InputCheckerTest.java
@@ -0,0 +1,24 @@
+package com.register.android.test;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class InputCheckerTest extends TestCase {
+
+	@Test
+	public void testCheckEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckPrice() {
+		fail("Not yet implemented");
+	}
+
+}
