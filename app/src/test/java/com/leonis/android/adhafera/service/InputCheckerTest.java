be1655f231f8e7f28d34e3b1000cda109a802d6c
Leonis0813 <Leonis.0813@gmail.com>
2018-02-13 23:58:16 +0900

    create InputCheckerTest

---
+++ app/src/test/java/com/leonis/android/adhafera/service/InputCheckerTest.java
@@ -0,0 +1,68 @@
+package com.leonis.android.adhafera.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by leonis on 2018/02/12.
+ *
+ */
+public class InputCheckerTest {
+    private InputChecker inputChecker;
+
+    public InputCheckerTest() {}
+
+    @Before
+    public void setUp() throws Exception {
+        inputChecker = new InputChecker();
+    }
+
+    @Test
+    public void testCheckEmpty_OK() {
+        String[] inputs = {"2015-01-01", "テスト用データ", "テスト", "100", "expense"};
+        assertTrue(inputChecker.checkEmpty(inputs).isEmpty());
+    }
+
+    @Test
+    public void testCheckEmpty_NG_priceIsNull() {
+        String[] inputs = {"2015-01-01", "テスト用データ", "テスト", null, "expense"};
+        ArrayList<Integer> ids = new ArrayList<>();
+        ids.add(3);
+        assertEquals(ids, inputChecker.checkEmpty(inputs));
+    }
+
+    @Test
+    public void testCheckEmpty_NG_allInputsAreNull() {
+        String[] inputs = {null, null, null, null, null};
+        ArrayList<Integer> ids = new ArrayList<>();
+        for(int i=0;i<inputs.length;i++) {
+            ids.add(i);
+        }
+        assertEquals(ids, inputChecker.checkEmpty(inputs));
+    }
+
+    @Test
+    public void testCheckDate_OK() {
+        assertTrue(inputChecker.checkDate("2015-01-01"));
+    }
+
+    @Test
+    public void testCheckDate_NG() {
+        assertFalse(inputChecker.checkDate("invalid"));
+    }
+
+    @Test
+    public void testCheckPrice_OK() {
+        assertTrue(inputChecker.checkPrice("100"));
+    }
+
+    @Test
+    public void testCheckPrice_NG() {
+        assertFalse(inputChecker.checkPrice("-100"));
+    }
+}
