fbbbaab7fd1ebb8f5bf589ad8b6ac771a50e698e
Leonis0813 <Leonis.0813@gmail.com>
2018-02-18 19:14:49 +0900

    create MainActivityTest

--- app/src/main/java/com/leonis/android/adhafera/view/RegistrationView.java
+++ app/src/main/java/com/leonis/android/adhafera/view/RegistrationView.java
@@ -2,7 +2,6 @@ package com.leonis.android.adhafera.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
